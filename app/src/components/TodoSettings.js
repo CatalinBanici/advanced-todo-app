96b84808bed6798abc17f19d27cdefb9d45474c9
Catalin Banici <catalinbanici95@yahoo.com>
2023-04-28 21:30:11 +0200

    mobile design and some more tweaks

--- app/src/components/TodoSettings.js
+++ app/src/components/TodoSettings.js
@@ -16,14 +16,65 @@ export default function TodoSettings(props) {
     setActiveFilterStyle(status);
   }
   return (
-    <aside>
-      <p className="items-left">
-        {todos.filter((todo) => !todo.complete).length}
-        {todos.filter((todo) => !todo.complete).length === 1
-          ? " item left"
-          : " items left"}
-      </p>
-      <div className="filter-container">
+    <>
+      <aside>
+        <div className="aside-container">
+          <p className="items-left">
+            {todos.filter((todo) => !todo.complete).length}
+            {todos.filter((todo) => !todo.complete).length === 1
+              ? " item left"
+              : " items left"}
+          </p>
+          <div className="filter-container">
+            <input
+              type="text"
+              placeholder="Search a todo..."
+              value={searchInput}
+              onChange={(e) => {
+                setSearchInput(e.target.value);
+                handleFilterOption("search");
+              }}
+            />
+            <div className="btn-filter-container">
+              <button
+                className={
+                  activeFilterStyle === "all"
+                    ? "btn-filter active"
+                    : "btn-filter"
+                }
+                onClick={() => handleFilterOption("all")}
+              >
+                All
+              </button>
+              <button
+                className={
+                  activeFilterStyle === "active"
+                    ? "btn-filter active"
+                    : "btn-filter"
+                }
+                onClick={() => handleFilterOption("active")}
+              >
+                Active
+              </button>
+              <button
+                className={
+                  activeFilterStyle === "completed"
+                    ? "btn-filter active"
+                    : "btn-filter"
+                }
+                onClick={() => handleFilterOption("completed")}
+              >
+                Completed
+              </button>
+            </div>
+          </div>
+          <button className="btn-clear" onClick={handleClearTodo}>
+            Clear Completed
+          </button>
+        </div>
+      </aside>
+
+      <div className="filter-container-2">
         <input
           type="text"
           placeholder="Search a todo..."
@@ -64,9 +115,6 @@ export default function TodoSettings(props) {
           </button>
         </div>
       </div>
-      <button className="btn-clear" onClick={handleClearTodo}>
-        Clear Completed
-      </button>
-    </aside>
+    </>
   );
 }
